e241adbbbbd51a92ede73bc23d6b9291de9163a5
Light <VerodiciaTheNatureGoddess@gmail.com>
2021-10-07 13:40:42 +0330

    Initial Project Setup

---
+++ VulkanRenderer/src/vkGraphicsContext.cpp
@@ -0,0 +1,1 @@
+#include "vkGraphicsContext.h"
---
+++ VulkanRenderer/src/vkGraphicsContext.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <volk.h>
+
+namespace Light {
+
+	class vkGraphicsContext
+	{
+	private:
+
+
+	public:
+		vkGraphicsContext() = default
+		~vkGraphicsContext() = default
+
+	};
+
+}
