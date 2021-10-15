42f3b73e959028b78a7815bb0aa606b41df1d8a6
Light <VerodiciaTheNatureGoddess@gmail.com>
2021-10-15 23:40:46 +0330

    Update Shader.cpp
    - Fixed Shader

--- VulkanRenderer/src/Shader.cpp
+++ VulkanRenderer/src/Shader.cpp
@@ -7,20 +7,35 @@ Shader::Shader(const std::string& vertexPath, const std::string& pixelPath, Shar
 	shaderc::SpvCompilationResult vertexResult = CompileGlslToSpv(vertexPath, Stage::VERTEX);
 	shaderc::SpvCompilationResult pixelResult = CompileGlslToSpv(pixelPath, Stage::PIXEL);
 
+	if (pixelResult.GetCompilationStatus() != shaderc_compilation_status_success)
+	{
+		LOG(err, "Shader::Shader: failed to compile vertex shader at {}", vertexPath);
+		LOG(err, "{}", vertexResult.GetErrorMessage());
+	}
+
+	if (pixelResult.GetCompilationStatus() != shaderc_compilation_status_success)
+	{
+		LOG(err, "Shader::Shader: failed to compile vertex shader at {}", pixelPath);
+		LOG(err, "{}", pixelResult.GetErrorMessage());
+	}
+
+	std::vector<uint32_t> vertexSpirv = { vertexResult.cbegin(), vertexResult.cend() };
+	std::vector<uint32_t> pixelSpirv = { pixelResult.cbegin(), pixelResult.cend() };
+
 	// vertex-shader module create-info
 	VkShaderModuleCreateInfo vertexShaderModuleCreateInfo
 	{
 		.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
-		.codeSize = static_cast<size_t>(vertexResult.cend() - vertexResult.cbegin()),
-		.pCode = vertexResult.cbegin()
+		.codeSize = vertexSpirv.size() * sizeof(uint32_t),
+		.pCode = vertexSpirv.data()
 	};
 
 	// pixel-shader module create-info
 	VkShaderModuleCreateInfo pixelShaderModuleCreateInfo
 	{
 		.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
-		.codeSize = static_cast<size_t>(pixelResult.cend() - pixelResult.cbegin()),
-		.pCode = pixelResult.cbegin()
+		.codeSize = pixelSpirv.size() * sizeof(uint32_t),
+		.pCode = pixelSpirv.data()
 	};
 
 	// create shader modules
@@ -58,16 +73,16 @@ Shader::~Shader()
 
 shaderc::SpvCompilationResult Shader::CompileGlslToSpv(const std::string& path, Stage stage)
 {
+	// compile options
 	shaderc::CompileOptions options;
 	options.SetTargetEnvironment(shaderc_target_env_vulkan, shaderc_env_version_vulkan_1_2);
 	options.SetOptimizationLevel(shaderc_optimization_level_performance);
 
-	shaderc::Compiler compiler;
-
+	// read shader file
 	std::ifstream file;
 	std::string sourceText = "";
 	std::string fileName = path.substr(path.find_last_of("/") + 1);
-	LOG(trace, std::filesystem::current_path().string());
+
 	file.open(path, std::ios_base::in | std::ios_base::binary);
 	ASSERT(file, "Shader::CompileGlslToSpv: failed to open {} shader file at: {}", stage == Stage::VERTEX ? "vertex" : "pixel", path);
 	if (file)
@@ -80,5 +95,7 @@ shaderc::SpvCompilationResult Shader::CompileGlslToSpv(const std::string& path, 
 		file.close();
 	}
 
+	// compile
+	shaderc::Compiler compiler;
 	return compiler.CompileGlslToSpv(sourceText, stage == Stage::VERTEX ? shaderc_shader_kind::shaderc_vertex_shader : shaderc_shader_kind::shaderc_fragment_shader, fileName.c_str(), options);
 }
