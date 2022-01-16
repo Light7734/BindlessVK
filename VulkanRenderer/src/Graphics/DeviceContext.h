26924df3718b0659779cb6f4934a5fc4fb632204
Light <Holypaladin47@gmail.com>
2022-01-16 21:33:31 +0330

    Fuck this I'm starting from scratch

--- VulkanRenderer/src/Graphics/DeviceContext.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <volk.h>
-
-struct DeviceContext
-{
-	VkDevice logical          = VK_NULL_HANDLE;
-	VkPhysicalDevice physical = VK_NULL_HANDLE;
-};
--- VulkanRenderer/src/clang-format-recursive.sh
+++
@@ -1,2 +0,0 @@
-find . -regex '.*\.\(cpp\|hpp\|cc\|cxx\|h\)' -exec clang-format -style=file -i --verbose {} \;
-read -p "Press any key to continue"
